8cf0b1eb74f8a9cce47bd11c5bf0664b68347ccc
Joelson <joelsonjose222@gmail.com>
2026-03-08 19:26:52 -0300

    atualização

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-function ativarLetra(elemento){
-    const arrTexto = elemento.innerHTML.split('');
-    console.log(arrTexto)
-    elemento.innerHTML = '';
-    arrTexto.forEach((letra, i) => {
-        setTimeout(() => {
-            elemento.innerHTML += letra;
-        }, 75 * i);
-    });
-}
 
-const titulo = document.querySelector('.digitando');
-ativarLetra(titulo);
+    function ativarLetra(elemento){
+        const arrTexto = elemento.innerHTML.split('');
+        console.log(arrTexto)
+        elemento.innerHTML = '';
+        arrTexto.forEach((letra, i) => {
+            setTimeout(() => {
+                elemento.innerHTML += letra;
+            }, 75 * i);
+        });
+    }
+
+    const titulo = document.querySelector('.digitando');
+    ativarLetra(titulo);
+
+const ativaMenu = document.querySelector('.fa-bars');
+
+ativaMenu.addEventListener('click', () =>{
+    ativaMenu.classList.toggle('fa-bars');
+    ativaMenu.classList.toggle('fa-xmark');
+})
